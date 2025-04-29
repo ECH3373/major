37244701a31b3efc101e1e82f114e73d3c00c3ba
37r4n <e.37r4n@gmail.com>
2025-04-29 15:34:02 -0600

    fix pwa

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,6 +6,7 @@ import { ToastProvider } from '@heroui/toast';
 export const metadata = {
   title: "MAJOR",
   manifest: "/manifest.json",
+
   icons: {
     icon: '/favicon.png',
     shortcut: '/favicon.png',
@@ -13,19 +14,21 @@ export const metadata = {
   }
 }
 
-export default function RootLayout({ children }) {
+export default function Layout({ children }) {
   return (
-    <html lang="es" className="dark">
-      <body>
-        <HeroUIProvider>
-          <DrawerProvider>
-            <SwalProvider>
-              <ToastProvider toastProps={{ timeout: 3000 }} />
-              {children}
-            </SwalProvider>
-          </DrawerProvider>
-        </HeroUIProvider>
-      </body>
-    </html>
+    <HeroUIProvider>
+      <DrawerProvider>
+        <SwalProvider>
+          <ToastProvider toastProps={{ timeout: 3000 }} />
+          <html lang="es" className="dark" >
+            <head>
+              <link rel="icon" href="/favicon.png" sizes="any" />
+              <link rel="manifest" href="/manifest.json" />
+            </head>
+            <body>{children}</body>
+          </html>
+        </SwalProvider>
+      </DrawerProvider>
+    </HeroUIProvider>
   );
 }
